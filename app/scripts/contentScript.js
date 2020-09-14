202260afd24353d4cc93236d19212560aadb7256
Riegler Daniel <daniel.riegler@essteyr.com>
2020-09-14 10:12:12 +0200

    Bit of code refactoring; still need to fix an issue when exporting approx. >50 labels

--- app/scripts/contentScript.js
+++ app/scripts/contentScript.js
@@ -24,30 +24,59 @@
 const { toBlob } = require('html-to-image');
 const JSZip = require('jszip');
 
+
+
 window.onload = () => {
     if (!document.title.includes('Labels'))
         return;
     window.URL = window.webkitURL || window.URL;
-    const labels = document.getElementsByClassName('label');
     const blobs = [];
-    new Promise(resolve => {
-        for (let index = 0; index < labels.length; index++) {
-            toBlob(labels[index]).then(blob => {
-                blobs.push(blob);
-                if (labels.length - 1 === index)
-                    resolve();
+    const labels = document.getElementsByClassName('label');
+    const download = () => {
+        new Promise(resolve => {
+            for (let index = 0; index < labels.length; index++)
+                toBlob(labels[index]).then(blob => {
+                    blobs.push(blob);
+                    if (labels.length - 1 === index)
+                        resolve();
+                })
+        }).catch(error => alert(error.message)).then(() => {
+            const zip = new JSZip();
+            blobs.forEach((blob, index) => {
+                zip.file(`label-${index + 1}.png`, blob);
+                if (blobs.length - 1 === index)
+                    zip.generateAsync({ type: 'blob' }).then(blob => {
+                        const button = document.createElement('a');
+                        button.href = URL.createObjectURL(blob);
+                        button.download = 'labels.zip';
+                        button.style.display = 'none';
+                        document.body.appendChild(button);
+                        button.click();
+                    });
             });
-        }
-    }).catch(error => alert(error.message)).then(() => {
-        const zip = new JSZip();
-        blobs.forEach((blob, index) => zip.file(`label-${index}.png`, blob));
-        zip.generateAsync({ type: 'blob' }).then(blob => {
-            const button = document.createElement('a');
-            button.href = URL.createObjectURL(blob);
-            button.download = 'labels.zip';
-            button.style.display = 'none';
-            document.body.appendChild(button);
-            button.click();
         });
+    };
+
+    const lineBreak = document.createElement('hr');
+    const closeButton = document.createElement('button');
+    const downloadButton = document.createElement('button');
+
+    closeButton.innerHTML = 'Hide';
+    closeButton.addEventListener('click', () => {
+        downloadButton.remove();
+        closeButton.remove();
+        lineBreak.remove();
     });
+
+    downloadButton.innerHTML = 'Download labels';
+    downloadButton.addEventListener('click', () => {
+        if (!downloadButton.disabled) {
+            downloadButton.disabled = true;
+            download();
+        }
+    });
+
+    document.body.prepend(lineBreak);
+    document.body.prepend(closeButton);
+    document.body.prepend(downloadButton);
 };
